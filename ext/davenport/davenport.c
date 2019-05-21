968af92971c50acec94c069bad08c5bd3302c198
Douglas Lovell <doug@wbreeze.com>
2019-05-21 12:36:13 -0300

    Add constructor and parameter type tests

--- ext/davenport/davenport.c
+++ ext/davenport/davenport.c
@@ -1,8 +1,17 @@
 #include <ruby.h>
+#include "davenport/davenport.h"
+#include "davenport/network.h"
 #include "extconf.h"
+
+static VALUE initialize(VALUE self, VALUE node_ct)
+{
+  Check_Type(node_ct, T_FIXNUM);
+  return self;
+}
 
 VALUE add_preference(VALUE self, VALUE pref_array)
 {
+  Check_Type(pref_array, T_ARRAY);
   return Qnil;
 }
 
@@ -20,6 +29,7 @@ void Init_davenport()
 {
   VALUE mDv = rb_define_module("Davenport");
   VALUE cPg = rb_define_class_under(mDv, "PreferenceGraph", rb_cObject);
+  rb_define_method(cPg, "initialize", initialize, 1);
   rb_define_method(cPg, "add_preference", add_preference, 1);
   rb_define_method(cPg, "davenport", davenport, 0);
 }
--- test/test_davenport.rb
+++ test/test_davenport.rb
@@ -8,9 +8,22 @@ class TestDavenport < Minitest::Test
 
   def test_one_ranking
     node_ct = 4;
-    pg = Davenport::PreferenceGraph.new  #(node_ct)
+    pg = Davenport::PreferenceGraph.new(node_ct)
     expected_ranking = [1, 3, 2, 4];
     pg.add_preference(expected_ranking);
     assert_equal(expected_ranking, pg.davenport);
+  end
+
+  def test_constructor_type_error
+    assert_raises(TypeError) do
+      Davenport::PreferenceGraph.new(Object.new)
+    end
+  end
+
+  def test_add_preference_type_error
+    pg = Davenport::PreferenceGraph.new(4)
+    assert_raises(TypeError) do
+      pg.add_preference(Object.new)
+    end
   end
 end
