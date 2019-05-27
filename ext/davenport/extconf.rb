9611708dfcbb69b22583dfa26a62480143de0705
Douglas Lovell <doug@wbreeze.com>
2019-05-27 15:11:35 -0300

    Require davenport library

--- ext/davenport/extconf.rb
+++ ext/davenport/extconf.rb
@@ -1,5 +1,9 @@
 #! ruby
 require 'mkmf'
 
+unless have_library('davenport', 'preference_graph_add_preference')
+  abort 'Need libdavenport, see https://github.com/wbreeze/davenport'
+end
+
 create_header
 create_makefile 'davenport/davenport'
